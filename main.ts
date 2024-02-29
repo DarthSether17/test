9fe1cfdcacff49d85761ec9199f842c1fe763ff7
DarthSether17 <156138144+DarthSether17@users.noreply.github.com>
2024-02-28 21:55:58 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@ namespace SpriteKind {
 }
 function Occupation3 (Occupation3Length: Sprite) {
     if (Occupation3Length.isHittingTile(CollisionDirection.Right)) {
+        direction = tiles.getTileLocation(currentLocation.column - 1, currentLocation.row)
+        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Left))
+        currentLocation = tiles.getTileLocation(currentLocation.column - 1, currentLocation.row)
         tiles.setTileAt(currentLocation, assets.tile`transparency16`)
         tiles.setWallAt(currentLocation, false)
-        direction = tiles.getTileLocation(currentLocation.column + 1, currentLocation.row)
-        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Right))
-        currentLocation = tiles.getTileLocation(currentLocation.column + 1, currentLocation.row)
     } else {
         tiles.setTileAt(currentLocation, assets.tile`myTile`)
         tiles.setWallAt(currentLocation, true)
@@ -16,62 +16,68 @@ function Occupation3 (Occupation3Length: Sprite) {
         tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Right))
         currentLocation = tiles.getTileLocation(currentLocation.column + 1, currentLocation.row)
     }
-    pause(200)
+    pause(40)
 }
 function Occupation1 (Ocupation1Length: Sprite) {
     if (Ocupation1Length.isHittingTile(CollisionDirection.Left)) {
+        currentLocation = tiles.getTileLocation(currentLocation.column + 1, currentLocation.row)
+        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Right))
         tiles.setTileAt(currentLocation, assets.tile`transparency16`)
         tiles.setWallAt(currentLocation, false)
-        direction = tiles.getTileLocation(currentLocation.column - 1, currentLocation.row)
-        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Left))
-        currentLocation = tiles.getTileLocation(currentLocation.column - 1, currentLocation.row)
     } else {
         tiles.setTileAt(currentLocation, assets.tile`myTile`)
         tiles.setWallAt(currentLocation, true)
-        direction = tiles.getTileLocation(currentLocation.column - 1, currentLocation.row)
         tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Left))
         currentLocation = tiles.getTileLocation(currentLocation.column - 1, currentLocation.row)
     }
-    pause(200)
+    pause(40)
 }
 function MakeMap (Mazy: number, Direction: number, length: number, extraNum: number, overlap: boolean) {
-    currentLocation = tiles.getTileLocation(touching.tilemapLocation().column, touching.tilemapLocation().row)
     maziness = Mazy
     length2 = length
     for (let index = 0; index < Mazy; index++) {
         if (maziness <= 3) {
-            length3 = length2 + 1
+            length3 = length2 + 4
         } else if (maziness <= 6) {
             for (let index = 0; index < 2; index++) {
-                length3 = length2 + 1
+                length3 = length2 + 4
             }
         } else if (maziness > 6) {
             for (let index = 0; index < 3; index++) {
-                length3 = length2 + 1
+                length3 = length2 + 4
             }
         }
-        tiles.placeOnTile(touching, currentLocation)
         Direction1 = randint(0, 3)
         for (let index = 0; index < length3; index++) {
+            tiles.placeOnTile(touching, tiles.getTileLocation(randint(1, 33), randint(1, 33)))
+            currentLocation = tiles.getTileLocation(touching.tilemapLocation().column, touching.tilemapLocation().row)
             if (Direction1 == 0) {
-                Occupation1(touching)
+                for (let index = 0; index < length3; index++) {
+                    Occupation1(touching)
+                }
             } else if (Direction1 == 1) {
-                Occupation2(touching)
+                for (let index = 0; index < length3; index++) {
+                    Occupation2(touching)
+                }
             } else if (Direction1 == 2) {
-                Occupation3(touching)
+                for (let index = 0; index < length3; index++) {
+                    Occupation3(touching)
+                }
             } else {
-                Occupation4(touching)
+                for (let index = 0; index < length3; index++) {
+                    Occupation4(touching)
+                }
             }
         }
     }
 }
 function Occupation2 (Occupation2Length: Sprite) {
     if (Occupation2Length.isHittingTile(CollisionDirection.Top)) {
+        direction = tiles.getTileLocation(currentLocation.column, currentLocation.row + 1)
+        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Bottom))
+        currentLocation = tiles.getTileLocation(currentLocation.column, currentLocation.row + 1)
         tiles.setTileAt(currentLocation, assets.tile`transparency16`)
         tiles.setWallAt(currentLocation, false)
-        direction = tiles.getTileLocation(currentLocation.column, currentLocation.row - 1)
-        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Top))
-        currentLocation = tiles.getTileLocation(currentLocation.column, currentLocation.row - 1)
     } else {
         tiles.setTileAt(currentLocation, assets.tile`myTile`)
         tiles.setWallAt(currentLocation, true)
@@ -79,15 +85,15 @@ function Occupation2 (Occupation2Length: Sprite) {
         tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Top))
         currentLocation = tiles.getTileLocation(currentLocation.column, currentLocation.row - 1)
     }
-    pause(200)
+    pause(40)
 }
 function Occupation4 (Occupation4Length: Sprite) {
     if (Occupation4Length.isHittingTile(CollisionDirection.Bottom)) {
+        direction = tiles.getTileLocation(currentLocation.column, currentLocation.row - 1)
+        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Top))
+        currentLocation = tiles.getTileLocation(currentLocation.column, currentLocation.row - 1)
         tiles.setTileAt(currentLocation, assets.tile`transparency16`)
         tiles.setWallAt(currentLocation, false)
-        direction = tiles.getTileLocation(currentLocation.column, currentLocation.row + 1)
-        tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Bottom))
-        currentLocation = tiles.getTileLocation(currentLocation.column, currentLocation.row + 1)
     } else {
         tiles.setTileAt(currentLocation, assets.tile`myTile`)
         tiles.setWallAt(currentLocation, true)
@@ -95,13 +101,13 @@ function Occupation4 (Occupation4Length: Sprite) {
         tiles.placeOnTile(touching, currentLocation.getNeighboringLocation(CollisionDirection.Bottom))
         currentLocation = tiles.getTileLocation(currentLocation.column, currentLocation.row + 1)
     }
-    pause(200)
+    pause(40)
 }
 let Direction1 = 0
 let length3 = 0
 let maziness = 0
-let direction: tiles.Location = null
 let currentLocation: tiles.Location = null
+let direction: tiles.Location = null
 let touching: Sprite = null
 let length2 = 0
 scene.setBackgroundImage(img`
@@ -246,8 +252,9 @@ touching = sprites.create(img`
     . . . . . . . . 3 3 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.builder)
-scene.cameraFollowSprite(touching)
 tiles.placeOnTile(touching, tiles.getTileLocation(randint(0, 35), randint(0, 35)))
+scene.cameraFollowSprite(touching)
+MakeMap(game.askForNumber("How dence with walls?", 1), randint(0, 3), randint(1, 12), 1, true)
 enum SpriteType {
     MyselfOne,
     MyselfTwo
@@ -290,12 +297,11 @@ let playerOne = sprites.create(img`
     `, SpriteType.MyselfOne)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), playerOne)
 mp.setPlayerIndicatorsVisible(true)
-Render.setViewMode(ViewMode.tilemapView)
+Render.setViewMode(ViewMode.raycastingView)
 Render.setViewAngleInDegree(90)
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
 // Set the initial position of the sprite
 PlayerOneImage.setPosition(playerOne.x, playerOne.y)
 PlayerOneImage.follow(playerOne, 1000)
-MakeMap(4, randint(0, 3), randint(1, 12), 1, true)
 scene.cameraFollowSprite(playerOne)
 sprites.destroy(touching)
